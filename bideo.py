81717c44398de219211d0371e40a6eda0f44a5fa
sylatupa <sylatupa@asu.edu>
2018-03-20 22:26:38 -0700

    eggs

--- bideo.py
+++ bideo.py
@@ -5,140 +5,6 @@ import socketserver
 from threading import Condition
 import http.server as server
 
-PAGE="""\
-<!DOCTYPE html>
-<html>
-  <head>
-    <header name = "Access-Control-Allow-Origin" value = "*" />
-      <meta http-equiv="Access-Control-Allow-Origin" content="*">
-      <meta content="en-us" http-equiv="Content-Language">
-      <meta charset="utf-8">
-      <meta content="An Interesting Title Goes Here" name="title">
-      <title>Raspberry Pi - Surveillance Camera</title>
-      <style type="text/css">
-body {
-  background-color: #FFF;
-  margin: 30px;
-  margin-top: 10px;
-}
-#contentContainer {
-  width: 640px;
-  height: 480px;
-  border: 5px black solid;
-  overflow: hidden;
-  background-color: #F2F2F2;
-  cursor: pointer;
-}
-#thing {
-  position: relative;
-  left: 50px;
-  top: 50px;
-  transition: left .5s cubic-bezier(.42,-0.3,.78,1.25), 
-    top .5s cubic-bezier(.42,-0.3,.78,1.25);
-}
-      </style>
-      <script>
-function rotate(dir){
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', '/rotate?'+dir);
-    xhr.send(null);
-}
-function image_effect(){
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', '/image_effect');
-    xhr.send(null);
-}
-function contrast(){
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', '/contrast');
-    xhr.send(null);
-}
-function saturation(){
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', '/saturation');
-    xhr.send(null);
-}
-function brightness(){
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', '/brightness');
-    xhr.send(null);
-}
-click=0
-x=0
-y=0
-w=1
-h=1
-function getClickPosition(e) {
-  var parentPosition = getPosition(e.currentTarget);
-  var xPosition = e.clientX - parentPosition.x - (theThing.clientWidth / 2);
-  var yPosition = e.clientY - parentPosition.y - (theThing.clientHeight / 2);
-  if(click==0){
-    x=  xPosition;
-    y= yPosition;
-    click=1
-  }
-  else{
-    click=0
-    w = xPosition - x;
-    h = yPosition - y;
-    var box = [x/640.0, y/480.0,w/640.0,h/480.0]
-    console.log(box);
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', '/chngCoor?'+box);
-    xhr.send(null);
-
-  }
-  theThing.style.left = xPosition + "px";
-  theThing.style.top = yPosition + "px";
-}
-
-// Helper function to get an element's exact position
-function getPosition(el) {
-  var xPos = 0;
-  var yPos = 0;
-  while (el) {
-    if (el.tagName == "BODY") {
-      var xScroll = el.scrollLeft || document.documentElement.scrollLeft;
-      var yScroll = el.scrollTop || document.documentElement.scrollTop;
-
-      xPos += (el.offsetLeft - xScroll + el.clientLeft);
-      yPos += (el.offsetTop - yScroll + el.clientTop);
-    } else {
-      // for all other non-BODY elements
-      xPos += (el.offsetLeft - el.scrollLeft + el.clientLeft);
-      yPos += (el.offsetTop - el.scrollTop + el.clientTop);
-    }
-
-    el = el.offsetParent;
-  }
-  return {
-    x: xPos,
-    y: yPos
-  };
-}
-      </script>
-      <body>
-	<img src="stream.mjpg"  >
-	<div id="contentContainer">
-          <img id="thing" alt="the thing" crossOrigin="anonymous" height="67" style="background:black;" width="67">
-	</div>
-<button onclick="rotate(10)">rotate right</button>
-<button onclick="rotate(-10)">rotate left</button>
-<button onclick="image_effect()">image_effect</button>
-<button onclick="contrast()">contrast</button>
-<button onclick="saturation()">saturation</button>
-<button onclick="brightness()">brightness</button>
-</body>
-      <script>
-var theThing = document.querySelector("#thing");
-var container = document.querySelector("#contentContainer");
-container.addEventListener("click", getClickPosition, false);
-      </script>
-</html>
-
-
-"""
-
 class StreamingOutput(object):
     def __init__(self):
         self.frame = None
@@ -261,7 +127,9 @@ class StreamingHandler(server.BaseHTTPRequestHandler):
                     'Removed streaming client %s: %s',
                     self.client_address, str(e))
         else:
-            self.send_error(404)
+            self.send_response(200)
+
+            #self.send_error(404)
             self.end_headers()
 
 class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
